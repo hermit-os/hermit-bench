966ce9126f797b9b2e4c35bb6d22f0e580544692
CarlWachter <carlwachter@web.de>
2025-07-03 21:55:27 +0200

    fix: output path to working dir

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,7 @@ fn main() -> io::Result<()> {
 
     // Write the results to the output file.
     let bench_json_str = serde_json::to_string_pretty(&results).unwrap();
-    let output_path = format!("{}/{}", relative_path, "../results.json");
+    let output_path = format!("{}/{}", relative_path, "results.json");
 
     std::fs::write(output_path, &bench_json_str).unwrap();
 

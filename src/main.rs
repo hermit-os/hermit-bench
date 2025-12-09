d2507484c6abf4b3ec949c08f980af003d7977fe
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-12-09 07:38:08 +0100

    refactor: use let-else in run_benchmark_command

--- src/main.rs
+++ src/main.rs
@@ -337,46 +337,43 @@ fn run_benchmark_command(benchmark: &Benchmark) -> String {
         .spawn()
         .expect("failed to execute process");
 
-    match child.wait_timeout(TIMEOUT).unwrap() {
-        Some(status) => {
-            // Get the output of the command, if it terminated before the timeout
-            let mut stdout = String::new();
-            if let Some(mut stdout_handle) = child.stdout.take() {
-                std::io::Read::read_to_string(&mut stdout_handle, &mut stdout).unwrap();
-            }
+    let Some(status) = child.wait_timeout(TIMEOUT).unwrap() else {
+        // The timeout elapsed
+        child.kill().unwrap();
 
-            // If the command failed, print the output and error
-            // Exit code 3 means success on QEMU's isa-debug-exit device on x86-64.
-            // Also disregard the error code 137, which parallel loves to throw
-            if !status.success() && !matches!(status.code().unwrap(), 3 | 137) {
-                let mut stderr = String::new();
-                if let Some(mut stderr_handle) = child.stderr.take() {
-                    std::io::Read::read_to_string(&mut stderr_handle, &mut stderr).unwrap();
-                }
-                eprintln!("Command failed with output: \n{stdout}\nAnd error: \n{stderr}");
-                eprintln!("Exit code: {}", status.code().unwrap());
-                panic!("Command failed");
-            }
-            stdout
+        // output the commands output message
+        let mut stdout = String::new();
+        if let Some(mut stdout_handle) = child.stdout.take() {
+            std::io::Read::read_to_string(&mut stdout_handle, &mut stdout).unwrap();
         }
-        None => {
-            // The timeout elapsed
-            child.kill().unwrap();
 
-            // output the commands output message
-            let mut stdout = String::new();
-            if let Some(mut stdout_handle) = child.stdout.take() {
-                std::io::Read::read_to_string(&mut stdout_handle, &mut stdout).unwrap();
-            }
-
-            let mut stderr = String::new();
-            if let Some(mut stderr_handle) = child.stderr.take() {
-                std::io::Read::read_to_string(&mut stderr_handle, &mut stderr).unwrap();
-            }
-            eprintln!("Command failed with output: \n{stdout}\nAnd error: \n{stderr}");
-            panic!("Command timed out");
+        let mut stderr = String::new();
+        if let Some(mut stderr_handle) = child.stderr.take() {
+            std::io::Read::read_to_string(&mut stderr_handle, &mut stderr).unwrap();
         }
+        eprintln!("Command failed with output: \n{stdout}\nAnd error: \n{stderr}");
+        panic!("Command timed out");
+    };
+
+    // Get the output of the command, if it terminated before the timeout
+    let mut stdout = String::new();
+    if let Some(mut stdout_handle) = child.stdout.take() {
+        std::io::Read::read_to_string(&mut stdout_handle, &mut stdout).unwrap();
     }
+
+    // If the command failed, print the output and error
+    // Exit code 3 means success on QEMU's isa-debug-exit device on x86-64.
+    // Also disregard the error code 137, which parallel loves to throw
+    if !status.success() && !matches!(status.code().unwrap(), 3 | 137) {
+        let mut stderr = String::new();
+        if let Some(mut stderr_handle) = child.stderr.take() {
+            std::io::Read::read_to_string(&mut stderr_handle, &mut stderr).unwrap();
+        }
+        eprintln!("Command failed with output: \n{stdout}\nAnd error: \n{stderr}");
+        eprintln!("Exit code: {}", status.code().unwrap());
+        panic!("Command failed");
+    }
+    stdout
 }
 
 fn run_pre_run_command(benchmark: &Benchmark) {
